d446639149e40a12f8bc09d5e310d0505f6beb1f
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2024-05-22 13:51:05 +0300

    new branch update on ui

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,19 +3,75 @@ import LogoSVG from '../../assets/lynk-in-houz.svg';
 import './Navbar.css';
 
 function Navbar() {
+  const navbarItems = [
+    {
+      id: 0,
+      title: 'Home',
+      link: '/',
+    },
+    {
+      id: 0,
+      title: 'About',
+      link: '/about',
+      items: [
+        { id: 0, title: 'Our History', link: '/' },
+        { id: 1, title: 'Contact us', link: '/' },
+      ],
+    },
+    {
+      id: 0,
+      title: 'Services',
+      link: '/services',
+      items: [
+        { id: 0, title: 'Pedicure', link: '/' },
+        { id: 1, title: 'Manicure', link: '/' },
+        { id: 2, title: 'Nail Art', link: '/' },
+        { id: 3, title: 'Nail Extension', link: '/' },
+      ],
+    },
+    {
+      id: 0,
+      title: 'Pages',
+      link: '/pages',
+      items: [
+        { id: 0, title: 'page 1', link: '/' },
+        { id: 1, title: 'page 2', link: '/' },
+        { id: 2, title: 'page 3', link: '/' }
+      ],
+    },
+    {
+      id: 0,
+      title: 'blog',
+      link: '/blog',
+    },
+    // ... other items
+  ];
+
   return (
     <nav>
-      <img src={LogoSVG} alt='mylogo' className='logo-container' />
+      <img src={LogoSVG} alt="mylogo" className="logo-container" />
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/pages">Pages</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        {navbarItems.map((item) => (
+          <li key={item.id} className="nav-item">
+            <Link to={item.link} style={{color: 'white'}}>{item.title}</Link>
+            {item.items && (
+              <ul className="dropdown-menu">
+                {item.items.map((link) => (
+                  <li key={link.id}>
+                    <Link to={link.link}>{link.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
       </ul>
-      <button className='book-now-btn'>
-      <li><a href="https://api.whatsapp.com/send?phone=+254701636709" target="_blank">Book Now</a></li>
+      <button className="book-now-btn">
+        <li>
+          <a href="https://api.whatsapp.com/send?phone=+254701636709" target="_blank" rel="noreferrer">
+            Book Now
+          </a>
+        </li>
       </button>
     </nav>
   );
